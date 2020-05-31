5cebc671f1f3c1a68d210cf6e03773cb2d5fe668
ftnext <takuyafjp+develop@gmail.com>
2020-05-31 14:53:19 +0900

    [feat] copy command MVP (worked but displayed timeout error at Slack)

--- cp_in_drive/lambda_function.py
+++ cp_in_drive/lambda_function.py
@@ -2,7 +2,12 @@ from datetime import datetime
 import hashlib
 import hmac
 import os
+import shutil
 from urllib import parse
+
+from operate_drive import create_diy_gdrive
+from operate_drive.drive import DiyGoogleDrive
+from operate_drive.file import DiyGDriveFile
 
 
 SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET")
@@ -25,8 +30,34 @@ def is_verified(headers, body):
     return True
 
 
+def cp_in_drive(source_id: str) -> DiyGDriveFile:
+    """Copy a specified file in Google Drive and return the created file."""
+    drive = create_diy_gdrive()
+    dest_title = build_dest_title(drive, source_id)
+    return drive.copy_file(source_id, dest_title)
+
+
+def build_dest_title(drive: DiyGoogleDrive, source_id: str) -> str:
+    source = drive.fetch_file_by_id(source_id)
+    source_title = source.fetch_title()
+    return title_of_copy_dest(source_title)
+
+
+def title_of_copy_dest(source_title: str) -> str:
+    # 暫定的に、元のファイル名にcopied_をつけるものとする
+    return f"copied_{source_title}"
+
+
 def lambda_handler(event, context):
     if not is_verified(event["headers"], event["body"]):
         return {"text": "Something went wrong..."}
-    text = parse.parse_qs(event["body"])["text"][0]
-    return {"text": f"Hello {text}"}
+    target_id = parse.parse_qs(event["body"])["text"][0]
+
+    # credentialをrefreshするため、書き込めるディレクトリ/tmp以下にコピーを作成
+    saved_secret_file = "saved_credentials.json"
+    saved_secret_tmp_path = "/tmp/" + saved_secret_file
+    shutil.copy2(saved_secret_file, saved_secret_tmp_path)
+
+    # TODO: 呼び出してから3秒で終了しないため、Slackではエラー表示。しかしコピーはできる
+    cp_in_drive(target_id)
+    return {"text": "Copying now..."}
