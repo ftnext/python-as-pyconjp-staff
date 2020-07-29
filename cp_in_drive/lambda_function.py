ec5076bfdd6ecc09b58bc90509d607c01d569f87
ftnext <takuyafjp+develop@gmail.com>
2020-07-29 23:02:26 +0900

    [feat] cp_in_drive lambda function for state machine

--- cp_in_drive/lambda_function.py
+++ cp_in_drive/lambda_function.py
@@ -1,33 +1,10 @@
-from datetime import datetime
-import hashlib
-import hmac
-import os
+import json
 import shutil
-from urllib import parse
+from urllib.request import Request, urlopen
 
 from operate_drive import create_diy_gdrive
 from operate_drive.drive import DiyGoogleDrive
 from operate_drive.file import DiyGDriveFile
-
-
-SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET")
-
-
-def is_verified(headers, body):
-    signature = headers["X-Slack-Signature"]
-    request_ts = int(headers["X-Slack-Request-Timestamp"])
-    now_ts = int(datetime.now().timestamp())
-    message = f"v0:{request_ts}:{body}"
-    hmac_obj = hmac.new(
-        bytes(SIGNING_SECRET, "UTF-8"), bytes(message, "UTF-8"), hashlib.sha256
-    )
-    expected = f"v0={hmac_obj.hexdigest()}"
-
-    if abs(request_ts - now_ts) > (60 * 5) or not hmac.compare_digest(
-        expected, signature
-    ):
-        return False
-    return True
 
 
 def cp_in_drive(source_id: str) -> DiyGDriveFile:
@@ -49,15 +26,19 @@ def title_of_copy_dest(source_title: str) -> str:
 
 
 def lambda_handler(event, context):
-    if not is_verified(event["headers"], event["body"]):
-        return {"text": "Something went wrong..."}
-    target_id = parse.parse_qs(event["body"])["text"][0]
+    target_id = event["target_id"]
+    response_url = event["response_url"]
 
     # credentialをrefreshするため、書き込めるディレクトリ/tmp以下にコピーを作成
     saved_secret_file = "saved_credentials.json"
     saved_secret_tmp_path = "/tmp/" + saved_secret_file
     shutil.copy2(saved_secret_file, saved_secret_tmp_path)
 
-    # TODO: 呼び出してから3秒で終了しないため、Slackではエラー表示。しかしコピーはできる
-    cp_in_drive(target_id)
-    return {"text": "Copying now..."}
+    copied_file = cp_in_drive(target_id)
+    access_url = copied_file.fetch_url()
+
+    message = {"text": f"Copied!\n{access_url}"}
+    body = json.dumps(message).encode()
+    request = Request(response_url, data=body)
+    with urlopen(request) as res:
+        return res.getcode()
