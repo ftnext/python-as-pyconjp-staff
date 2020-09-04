3198dd9aa75c411e945128639e34a1d57cfd62fa
ftnext <takuyafjp+develop@gmail.com>
2020-09-04 23:05:44 +0900

    [feat] before deploy function, create layers as needed

--- cli_boilerplate/spread_slack_lambda/deploy.sh
+++ cli_boilerplate/spread_slack_lambda/deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PROJECT_DIR=$(dirname $(dirname ${SCRIPT_DIR}))
 
 usage() {
   echo "Usage: $0 -n <lambda_function_name> -r <role_arn> -e <env_var_pair> [-e <env_var_pair>] [-- --profile foo]" 1>&2
@@ -53,6 +54,12 @@ if [ ${#ENVIRONMENT_VARIABLES[@]} -eq 0 ]; then
   usage
 fi
 
+grep_from_json() {
+  json_string=$1
+  keyword=$2
+  echo ${json_string} | python3 -m json.tool | grep ${keyword}
+}
+
 strip_trailing_comma() {
   echo $1 | sed -E 's/,$//'
 }
@@ -61,15 +68,57 @@ strip_double_quotes() {
   echo $1 | sed -E 's/^"//' | sed -E 's/"$//'
 }
 
+publish_lambda_layer() {
+  layer_zip=$1
+  layer_package=$2
+  layer_name=$3
+  layer_description=$4
+  shift 4
+  echo $@
+
+  cd ${PROJECT_DIR}/layer_factory
+  rm build/${layer_zip}
+  ./create_layer_zip.sh -p ${layer_package} -l ${layer_zip}
+  cd build/
+  aws lambda publish-layer-version \
+    --layer-name ${layer_name} \
+    --compatible-runtimes python3.8 \
+    --description python38_${layer_description} \
+    --zip-file fileb://${layer_zip} \
+    $@
+}
+
+# set -eだとgrepで見つからないときstatusは0以外が返るが、スクリプトがエラー終了しないように緩める
+set +e
+
+LIST_LAYER_ARNS=$(aws lambda list-layers --query 'Layers[*].LatestMatchingVersion.LayerVersionArn' $@)
+
+POSTSLACK_LAYER_ARN=$(grep_from_json "${LIST_LAYER_ARNS}" postslack)
+if [ "${POSTSLACK_LAYER_ARN}" = "" ]; then
+  echo "postslack layer does not exist yet. Publish postslack layer ..."
+  publish_lambda_layer "python38_diypostslack01_layer.zip" \
+    git+https://github.com/ftnext/diy-slack-post@master#egg=postslack \
+    diy-postslack diypostslack01 $@
+fi
+
+GSPREAD_LAYER_ARN=$(grep_from_json "${LIST_LAYER_ARNS}" gspread)
+if [ "${GSPREAD_LAYER_ARN}" = "" ]; then
+  echo "gspread layer does not exist yet. Publish gspread layer ..."
+  publish_lambda_layer "python38_gspread36_layer.zip " \
+    gspread==3.6.0 gspread36 gspread36 $@
+fi
+
+set -e
+
 LAYER_ARNS=()
 LIST_LAYER_ARNS=$(aws lambda list-layers --query 'Layers[*].LatestMatchingVersion.LayerVersionArn' $@)
 
-POSTSLACK_LAYER_ARN=$(echo ${LIST_LAYER_ARNS} | python3 -m json.tool | grep postslack)
+POSTSLACK_LAYER_ARN=$(grep_from_json "${LIST_LAYER_ARNS}" postslack)
 POSTSLACK_LAYER_ARN=$(strip_trailing_comma ${POSTSLACK_LAYER_ARN})
 POSTSLACK_LAYER_ARN=$(strip_double_quotes ${POSTSLACK_LAYER_ARN})
 LAYER_ARNS+=(${POSTSLACK_LAYER_ARN})
 
-GSPREAD_LAYER_ARN=$(echo ${LIST_LAYER_ARNS} | python3 -m json.tool | grep gspread)
+GSPREAD_LAYER_ARN=$(grep_from_json "${LIST_LAYER_ARNS}" gspread)
 GSPREAD_LAYER_ARN=$(strip_trailing_comma ${GSPREAD_LAYER_ARN})
 GSPREAD_LAYER_ARN=$(strip_double_quotes ${GSPREAD_LAYER_ARN})
 LAYER_ARNS+=(${GSPREAD_LAYER_ARN})
