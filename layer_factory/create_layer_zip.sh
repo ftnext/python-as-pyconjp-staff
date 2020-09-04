ea7a3a71e0c838d6476c2f1a61a22664ff1472c1
ftnext <takuyafjp+develop@gmail.com>
2020-09-04 23:40:49 +0900

    [bugfix] fix redirect to stderr in layer_factory script

--- layer_factory/create_layer_zip.sh
+++ layer_factory/create_layer_zip.sh
@@ -4,12 +4,12 @@ set -eu
 usage() {
   echo "Usage: $0 -p <python_package> -l <layer_zip_file_name>" 1>&2
   echo "" 1>&2
-  echo "Create a zip file for Lambda layers in _build_ dir from the specified package."
+  echo "Create a zip file for Lambda layers in _build_ dir from the specified package." 1>&2
   echo "" 1>&2
   echo "-p: Specify Python package in PyPI or open git repository. Required" 1>&2
   echo "-l: Specify created zip file name. Required" 1>&2
-  echo "-x: Be verbose."
-  echo "-h: Show this help message."
+  echo "-x: Be verbose." 1>&2
+  echo "-h: Show this help message." 1>&2
   exit 1
 }
 
