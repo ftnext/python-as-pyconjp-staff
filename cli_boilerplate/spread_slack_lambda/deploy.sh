34f3555b76182933a92e1809e43f14de7690ba12
ftnext <takuyafjp+develop@gmail.com>
2020-09-04 21:52:49 +0900

    [feat] remove -l option; automate fetching layer ARN

--- cli_boilerplate/spread_slack_lambda/deploy.sh
+++ cli_boilerplate/spread_slack_lambda/deploy.sh
@@ -5,13 +5,12 @@ set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 usage() {
-  echo "Usage: $0 -n <lambda_function_name> -r <role_arn> -l <layer_arn> [-l <layer_arn>] -e <env_var_pair> [-e <env_var_pair>] [-- --profile foo]" 1>&2
+  echo "Usage: $0 -n <lambda_function_name> -r <role_arn> -e <env_var_pair> [-e <env_var_pair>] [-- --profile foo]" 1>&2
   echo "" 1>&2
   echo "Deploy AWS Lambda function which can read/write Google spreadsheet and post to Slack." 1>&2
   echo "" 1>&2
   echo "-n: Specify name of the Lambda function. Required" 1>&2
   echo "-r: Specify ARN of role for the Lambda function. Required" 1>&2
-  echo "-l: Specify ARN of layer for the Lambda function. Required at least one" 1>&2
   echo "-e: Specify environment variable as key=value format. Required at least one" 1>&2
   echo "-x: Be verbose." 1>&2
   echo "-h: Show this help message." 1>&2
@@ -24,17 +23,14 @@ if [ "$1" = "--help" -o "$1" = "-help" ]; then
   usage
 fi
 
-LAYER_ARNS=()
 ENVIRONMENT_VARIABLES=()
 
-while getopts n:r:l:e:xh OPT
+while getopts n:r:e:xh OPT
 do
   case $OPT in
     "n" ) FUNCTION_NAME=$OPTARG
       ;;
     "r" ) ROLE_ARN=$OPTARG
-      ;;
-    "l" ) LAYER_ARNS+=($OPTARG)
       ;;
     "e" ) ENVIRONMENT_VARIABLES+=($OPTARG)
       ;;
@@ -53,17 +49,37 @@ if [ "${FUNCTION_NAME}" = "" -o "${ROLE_ARN}" = "" ]; then
   usage
 fi
 
-if [ ${#LAYER_ARNS[@]} -eq 0 -o ${#ENVIRONMENT_VARIABLES[@]} -eq 0 ]; then
+if [ ${#ENVIRONMENT_VARIABLES[@]} -eq 0 ]; then
   usage
 fi
+
+strip_trailing_comma() {
+  echo $1 | sed -E 's/,$//'
+}
+
+strip_double_quotes() {
+  echo $1 | sed -E 's/^"//' | sed -E 's/"$//'
+}
+
+LAYER_ARNS=()
+LIST_LAYER_ARNS=$(aws lambda list-layers --query 'Layers[*].LatestMatchingVersion.LayerVersionArn' $@)
+
+POSTSLACK_LAYER_ARN=$(echo ${LIST_LAYER_ARNS} | python3 -m json.tool | grep postslack)
+POSTSLACK_LAYER_ARN=$(strip_trailing_comma ${POSTSLACK_LAYER_ARN})
+POSTSLACK_LAYER_ARN=$(strip_double_quotes ${POSTSLACK_LAYER_ARN})
+LAYER_ARNS+=(${POSTSLACK_LAYER_ARN})
+
+GSPREAD_LAYER_ARN=$(echo ${LIST_LAYER_ARNS} | python3 -m json.tool | grep gspread)
+GSPREAD_LAYER_ARN=$(strip_trailing_comma ${GSPREAD_LAYER_ARN})
+GSPREAD_LAYER_ARN=$(strip_double_quotes ${GSPREAD_LAYER_ARN})
+LAYER_ARNS+=(${GSPREAD_LAYER_ARN})
 
 ENVIRONMENT_VARIABLES_STRING=""
 for env_var in ${ENVIRONMENT_VARIABLES[@]}
 do
   ENVIRONMENT_VARIABLES_STRING="${env_var},${ENVIRONMENT_VARIABLES_STRING}"
 done
-# remove trailing comma
-ENVIRONMENT_VARIABLES_STRING=$(echo ${ENVIRONMENT_VARIABLES_STRING} | sed -E 's/,$//')
+ENVIRONMENT_VARIABLES_STRING=$(strip_trailing_comma ${ENVIRONMENT_VARIABLES_STRING})
 
 # zipにフルパス分のディレクトリができないようにcdすることにした
 cd ${SCRIPT_DIR}
