f48b948c6967f5cdc1743385a03f7d8442951415
ftnext <takuyafjp+develop@gmail.com>
2020-11-03 20:56:28 +0900

    [feat] enable to list subissues of each epics under the specified component

--- knowledge_base_helper/main.py
+++ knowledge_base_helper/main.py
@@ -1,5 +1,6 @@
 import os
 from argparse import ArgumentParser
+from collections import defaultdict
 
 from jira import JIRA
 
@@ -21,6 +22,11 @@ def authorize_client():
     return JIRA(options, basic_auth=(email, api_token))
 
 
+def fetch_issues_by_component(jira_client, component):
+    jql = f"project=NEZ and component in ({args.component}) order by key asc"
+    return jira_client.search_issues(jql, maxResults=False)
+
+
 def fetch_epics_by_component(jira_client, component):
     jql = (
         f"project=NEZ and component in ({args.component}) and issuetype=Epic "
@@ -35,10 +41,35 @@ def print_epics_as_bullet_line(epics):
         print(f"  * ref: {epic.permalink()}")
 
 
+def print_epic_subissues_as_bullet_line(epics, epic_to_subissues_map):
+    for epic in epics:
+        epic_key = epic.key
+        print(f"* {epic.fields.summary} ({epic_key})")
+        for subissue in epic_to_subissues_map[epic_key]:
+            print(f"  * {subissue.fields.summary} ({subissue.key})")
+
+
 def list_epics(args):
     jira = authorize_client()
     epics_under_components = fetch_epics_by_component(jira, args.component)
     print_epics_as_bullet_line(epics_under_components)
+
+
+def list_epic_subissues(args):
+    jira = authorize_client()
+    epics_under_components = fetch_epics_by_component(jira, args.component)
+    key_to_epic_map = {epic.key: epic for epic in epics_under_components}
+
+    issues_under_components = fetch_issues_by_component(jira, args.component)
+    epic_to_subissues_map = defaultdict(list)
+    for issue in issues_under_components:
+        parent_key = issue.fields.customfield_10008
+        if parent_key in key_to_epic_map:  # if parent is epic
+            epic_to_subissues_map[parent_key].append(issue)
+
+    print_epic_subissues_as_bullet_line(
+        key_to_epic_map.values(), epic_to_subissues_map
+    )
 
 
 if __name__ == "__main__":
@@ -48,6 +79,9 @@ if __name__ == "__main__":
 
     list_epics_parser = subparsers.add_parser("list_epics")
     list_epics_parser.set_defaults(func=list_epics)
+
+    list_epic_subissues_parser = subparsers.add_parser("list_epic_subissues")
+    list_epic_subissues_parser.set_defaults(func=list_epic_subissues)
 
     args = parser.parse_args()
     args.func(args)
