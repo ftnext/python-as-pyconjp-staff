2e561a2fbeb137666286ab9ac662fa1ec0bfb3b6
ftnext <takuyafjp+develop@gmail.com>
2020-12-14 21:04:42 +0900

    [feat] enable to get page contents and trim ALL html tags

--- knowledge_base_checker/main.py
+++ knowledge_base_checker/main.py
@@ -2,10 +2,16 @@ import os
 import sys
 
 from atlassian import Confluence
+from bs4 import BeautifulSoup
 
 
 def verify_environment_variables():
     return os.getenv("ATLASSIAN_EMAIL") and os.getenv("ATLASSIAN_API_TOKEN")
+
+
+def trim_html_tags(content):
+    soup = BeautifulSoup(content, "html.parser")
+    return soup.get_text()
 
 
 if __name__ == "__main__":
@@ -20,3 +26,14 @@ if __name__ == "__main__":
         username=os.getenv("ATLASSIAN_EMAIL"),
         password=os.getenv("ATLASSIAN_API_TOKEN"),
     )
+
+    space_content = confluence.get_space_content("pyconjp")
+    pages = space_content["page"]["results"]
+    print(f"{len(pages)} pages")
+    print("-" * 40)
+
+    for page in pages:
+        title = page["title"]
+        html_body = page["body"]["storage"]["value"]
+        content = trim_html_tags(html_body)
+        print(f"{title} ({len(content)} characters)")
