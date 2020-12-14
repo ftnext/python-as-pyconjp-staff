a32c02b94c89847aa34b1cb07b37bf59cbb62dda
ftnext <takuyafjp+develop@gmail.com>
2020-12-15 01:26:39 +0900

    [feat] enable to extract suspicious links from Confluence contents

--- knowledge_base_checker/main.py
+++ knowledge_base_checker/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 
 from atlassian import Confluence
@@ -9,9 +10,14 @@ def verify_environment_variables():
     return os.getenv("ATLASSIAN_EMAIL") and os.getenv("ATLASSIAN_API_TOKEN")
 
 
-def trim_html_tags(content):
-    soup = BeautifulSoup(content, "html.parser")
+def trim_html_tags(html):
+    soup = BeautifulSoup(html, "html.parser")
     return soup.get_text()
+
+
+def extract_anchor_urls(html):
+    soup = BeautifulSoup(html, "html.parser")
+    return [link.get("href") for link in soup.find_all("a")]
 
 
 if __name__ == "__main__":
@@ -37,3 +43,10 @@ if __name__ == "__main__":
         html_body = page["body"]["storage"]["value"]
         content = trim_html_tags(html_body)
         print(f"{title} ({len(content)} characters)")
+
+        urls = extract_anchor_urls(html_body)
+        for url in urls:
+            # 共有ドライブへの直リンクと思しきリンクを洗い出す
+            if re.match(r"https?://(docs|drive).google.com/.*", url):
+                print(url)
+        print("-" * 30)
