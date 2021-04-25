e80c2694fb1dbcf05cb3dcd7edd489a00dcb62f0
ftnext <takuyafjp+develop@gmail.com>
2021-04-25 10:57:51 +0900

    [test] timetable as yaml: Add test of Program.__eq__

--- timetable_maker/tests/timetable/test_data.py
+++ timetable_maker/tests/timetable/test_data.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from unittest import TestCase
 
 from timetable.data import Program
@@ -10,3 +11,13 @@ class ProgramTestCase(TestCase):
                 actual = Program.zero_filled_hour(time_str)
 
                 self.assertEqual(actual, expected)
+
+    def test_eq_between_program_instances(self):
+        """start時刻が等しければ、プログラムとして等しいとみなす"""
+        program1 = Program(dt.time(10, 0), dt.time(11, 0), "Test1", ["Room1"])
+        # yamlファイルではstart時刻が等しいプログラムは書かれない想定
+        program2 = Program(
+            dt.time(10, 0), dt.time(10, 30), "Test2", ["Room2", "Room3"]
+        )
+
+        self.assertTrue(program1 == program2)
