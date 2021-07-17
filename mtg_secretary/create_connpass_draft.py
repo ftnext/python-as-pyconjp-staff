7d96deeb029668bd9434a32fa78ac2792a42e07c
ftnext <takuyafjp+develop@gmail.com>
2021-07-17 16:21:56 +0900

    [feat] Implement login subcommand to login one command

--- mtg_secretary/create_connpass_draft.py
+++ mtg_secretary/create_connpass_draft.py
@@ -1,3 +1,30 @@
+import argparse
+
 from helium import kill_browser
 
-kill_browser()
+from connpass.playbook import login
+
+
+def do_login(args):
+    login()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--kill_browser", action="store_true")
+    subparsers = parser.add_subparsers(title="subcommand")
+
+    login_parser = subparsers.add_parser("login")
+    login_parser.set_defaults(func=do_login)
+
+    args = parser.parse_args()
+
+    args.func(args)
+
+    if args.kill_browser:
+        kill_browser()
+    else:
+        while True:
+            if input("qで終了: ") == "q":
+                kill_browser()
+                break
