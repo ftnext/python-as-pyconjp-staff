3972e352419dafd06d40550afcc91ff840c68bd5
ftnext <takuyafjp+develop@gmail.com>
2021-07-19 23:48:35 +0900

    [docs] Add help message as documentation

--- mtg_secretary/draft_connpass.py
+++ mtg_secretary/draft_connpass.py
@@ -19,20 +19,39 @@ def do_draft(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    description = "Connpass event drafter by browser automation."
+    parser = argparse.ArgumentParser(description=description)
     common_options_parser = argparse.ArgumentParser(add_help=False)
-    common_options_parser.add_argument("--kill_browser", action="store_true")
+    common_options_parser.add_argument(
+        "--kill_browser",
+        action="store_true",
+        help=(
+            "Specify if you want to close browser window after operation "
+            "(default: not specified, which means keeping the window open)."
+        ),
+    )
     subparsers = parser.add_subparsers(title="subcommand")
 
     login_parser = subparsers.add_parser(
-        "login", parents=[common_options_parser]
+        "login",
+        parents=[common_options_parser],
+        description="Log in to connpass.",
     )
     login_parser.set_defaults(func=do_login)
 
     draft_parser = subparsers.add_parser(
-        "draft", parents=[common_options_parser]
+        "draft",
+        parents=[common_options_parser],
+        description="Draft connpass event with template after login.",
     )
-    draft_parser.add_argument("event_info", type=Path)
+    draft_parser.add_argument(
+        "event_info",
+        type=Path,
+        help=(
+            "Specify JSON file with represents event information "
+            "(See example)."
+        ),
+    )
     draft_parser.set_defaults(func=do_draft)
 
     args = parser.parse_args()
