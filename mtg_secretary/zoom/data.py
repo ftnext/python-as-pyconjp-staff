84f12c49040d0c370b4e0b42ebe786af743299d4
ftnext <takuyafjp+develop@gmail.com>
2021-08-15 14:37:43 +0900

    [feat] Implement ScheduledMeeting's factory (close #45)

--- mtg_secretary/zoom/data.py
+++ mtg_secretary/zoom/data.py
@@ -1,4 +1,24 @@
+import datetime as dt
+from dataclasses import dataclass
+
+import pytz
+
+ASIA_TOKYO = pytz.timezone("Asia/Tokyo")
+
+
+@dataclass
 class ScheduledMeeting:
+    datetime: dt.datetime
+    topic: str
+    url: str
+
     @classmethod
-    def create(cls, datetime_string, topic, url):
-        raise NotImplementedError
+    def create(
+        cls, datetime_string: str, topic: str, url: str
+    ) -> "ScheduledMeeting":
+        naive_dt = dt.datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%SZ")
+        jst_dt = ASIA_TOKYO.localize(naive_dt)
+        jst_naive_dt = (jst_dt + jst_dt.tzinfo.utcoffset(jst_dt)).replace(
+            tzinfo=None
+        )
+        return cls(jst_naive_dt, topic, url)
