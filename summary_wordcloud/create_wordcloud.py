5dca4466d72c13391e6012759fdeedffa9bce7fd
ftnext <takuyafjp+develop@gmail.com>
2021-10-13 22:25:06 +0900

    [feat] Specify parameters of wordcloud from command line

--- summary_wordcloud/create_wordcloud.py
+++ summary_wordcloud/create_wordcloud.py
@@ -15,6 +15,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_text")
     parser.add_argument("output_image")
+    parser.add_argument("--font_path", default="ipagp.ttf")
+    parser.add_argument("--random_state", type=int, default=1)
+    parser.add_argument("--max_words", type=int, default=30)
+    parser.add_argument("--width", type=int, default=1024)
+    parser.add_argument("--height", type=int, default=674)
     args = parser.parse_args()
 
     t = Tokenizer()
@@ -30,12 +35,12 @@ if __name__ == "__main__":
         tokens = a.analyze(text)
 
     wordcloud = WordCloud(
-        font_path="ipagp.ttf",
+        font_path=args.font_path,
         background_color="white",
-        width=1024,
-        height=674,
+        width=args.width,
+        height=args.height,
         collocations=False,
-        random_state=1,
-        max_words=30,
+        random_state=args.random_state,
+        max_words=args.max_words,
     ).generate(" ".join(tokens))
     wordcloud.to_image().save(args.output_image)
